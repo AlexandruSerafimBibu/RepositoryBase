b214ce93d4d6f9b027eb9258f2554aa87b719a85
Bibu Serafim <alexandru.serafim.bibu@gmail.com>
2022-04-28 12:17:15 +0300

    first commit

---
+++ RepositoryBase/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace RepositoryBase;
+
+public interface IRepositoryBase<TEntity, K>
+    where TEntity : class
+{
+    ValueTask<TEntity> FindAsync(K id);
+    Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate);
+    Task<IList<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter, int skip, int take, bool noTracking, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy, Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>> include);
+    Task<int> CountAsync(Expression<Func<TEntity, bool>> filter);
+}
---
+++ RepositoryBase/RepositoryBase.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace RepositoryBase;
+
+public class RepositoryBase<T, C, K> : IRepositoryBase<T, K>
+        where T : class
+        where C : DbContext
+{
+    protected readonly DbSet<T> _set;
+    protected readonly DbContext _dbContext;
+
+    public RepositoryBase(C dbContext)
+    {
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        _set = dbContext.Set<T>();
+    }
+
+    public async ValueTask<T> FindAsync(K id)
+    {
+        return await _set.FindAsync(id);
+    }
+
+    public async Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> filter, int skip, int take, bool noTracking, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy, Func<IQueryable<T>, IIncludableQueryable<T, object>> include)
+    {
+        IQueryable<T> query = noTracking ? _set.AsNoTracking().AsQueryable() : _set.AsQueryable();
+        
+        if(filter != null)
+        {
+            query = query.Where(filter);
+        }
+
+        if (include != null)
+        {
+            query = include(query);
+        }
+        if (orderBy != null)
+        {
+            query = orderBy(query).AsQueryable();
+        }
+        query = skip == 0 ? query.Take(take) : query.Skip(skip).Take(take);
+
+        return await query.ToListAsync();
+    }
+
+
+}
